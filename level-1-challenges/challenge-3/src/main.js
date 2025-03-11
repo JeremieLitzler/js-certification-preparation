bcd8f8c50eb7c9aafa0673eed05dd2cba2ccd2c8
Jeremie Litzler <jlitzler@cbtw.tech>
2025-03-11 16:47:45 +0100

    complete level 1 - challenge 3

---
+++ level-1-challenges/challenge-3/src/main.js
@@ -0,0 +1,27 @@
+// Write your code here! 👇
+const jsIsAwesome = true;
+console.log(typeof jsIsAwesome);
+
+const firstName = 'Jeremie';
+const lastName = 'Litzler';
+const fullName = `${firstName} ${lastName}`;
+console.log(firstName);
+console.log(lastName);
+console.log(fullName);
+console.log(typeof fullName);
+
+const age = 37;
+console.log(age);
+console.log(typeof age);
+
+const isOlderThan50 = age > 50;
+console.log(isOlderThan50);
+console.log(typeof isOlderThan50);
+
+let nothingToSeeHere;
+console.log(nothingToSeeHere);
+console.log(typeof nothingToSeeHere);
+
+const reasonsNotToLearnJavaScript = null;
+console.log(reasonsNotToLearnJavaScript);
+console.log(typeof reasonsNotToLearnJavaScript);
