e6a8e2f1d28f03a5757c3e4bbb14763bc35c0a74
Jeremie Litzler <jeremie.litzler@gmail.com>
2025-03-14 08:50:48 +0100

    complete challenge 19 on level 1

--- level-1-challenges/challenge-19/CHECKLIST.md
+++ level-1-challenges/challenge-19/CHECKLIST.md
@@ -1,1 +1,15 @@
-- The console displays the expected output
+## What to Expect
+
+Your output should look like this:
+
+```
+Even numbers: 4
+Odd numbers: 6
+Low numbers: 3
+Medium numbers: 5
+High numbers: 2
+Sum: 466
+Average: 46.60
+```
+
+> 💡 HINT: Remember to use appropriate loop structures (for or while) and conditional statements (if-else) to implement the required logic. Pay attention to the initial value, condition, and increment of your loops.
--- level-1-challenges/challenge-19/src/main.js
+++ level-1-challenges/challenge-19/src/main.js
@@ -1,1 +1,39 @@
 // complete the challenge here 👇
+const numbers = [23, 54, 32, 87, 47, 15, 98, 6, 63, 41];
+
+let even = 0;
+let odd = 0;
+for (let index = 0; index < numbers.length; index++) {
+  const number = numbers[index];
+  if (number % 2 === 0) {
+    even++;
+  } else {
+    odd++;
+  }
+}
+console.log(`Even numbers: ${even}`);
+console.log(`Odd numbers: ${odd}`);
+
+let low = 0;
+let medium = 0;
+let high = 0;
+for (let index = 0; index < numbers.length; index++) {
+  const number = numbers[index];
+  if (number < 30) {
+    low++;
+  } else if (number >= 30 && number <= 70) {
+    medium++;
+  } else if (number > 70) {
+    high++;
+  }
+}
+console.log(`Low numbers: ${low}`);
+console.log(`Medium numbers: ${medium}`);
+console.log(`High numbers: ${high}`);
+
+let sum = 0;
+numbers.forEach((number) => (sum += number));
+console.log(`Sum: ${sum}`);
+
+const average = (sum / numbers.length).toFixed(2);
+console.log(`Average: ${average}`);
