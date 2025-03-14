ab7d4703e3c582e13d31590320df58d4db4c1160
Jeremie Litzler <jeremie.litzler@gmail.com>
2025-03-14 09:35:05 +0100

    complete challenge 20 on level 1

--- level-1-challenges/challenge-20/CHECKLIST.md
+++ level-1-challenges/challenge-20/CHECKLIST.md
@@ -1,1 +1,21 @@
-- The console displays the expected output
+## What to Expect
+
+Your output should look similar to this:
+
+```
+A mystical brew of water, moonstone dust, and dragon breath!
+A mystical brew of witch hazel, moonstone dust, and dragon breath!
+A mystical brew of toadstool extract, pixie wings, and dragon breath!
+A mystical brew of shadow essence, vampire tears, and phoenix ash!
+May luck and fortune smile upon Elara the Elf!
+Spell Power: 250
+Fireball blazes with power 1!
+Water spout surges with power 2!
+Lightning strikes with power 3!
+Weak
+Moderate
+Strong
+(4) ['enchanted newt eyes', 'enchanted toe of frog', 'enchanted wool of bat', 'enchanted tongue of dog']
+```
+
+> 💡 HINT: Remember that spells (functions) can be assigned to variables, passed as arguments to other spells, and stored in magical tomes (arrays). Use loops to repeat actions and conditionals to make decisions based on magical energies (values).
--- level-1-challenges/challenge-20/src/main.js
+++ level-1-challenges/challenge-20/src/main.js
@@ -1,1 +1,59 @@
 // complete the challenge here 👇
+
+function createPotion(
+  baseIngredient = 'water',
+  catalystIngredient = 'moonstone dust',
+  powerBooster = 'dragon breath'
+) {
+  return `A mystical brew of ${baseIngredient}, ${catalystIngredient}, and ${powerBooster}!`;
+}
+
+console.log(createPotion());
+console.log(createPotion('witch hazel'));
+console.log(createPotion('toadstool extract', 'pixie wings'));
+console.log(createPotion('shadow essence', 'vampire tears', 'phoenix ash'));
+
+const castCharm = function (name) {
+  return `May luck and fortune smile upon ${name}!`;
+};
+
+console.log(castCharm('Elara the Elf'));
+
+const calculateSpellPower = (age) => {
+  return age * 10;
+};
+
+console.log(calculateSpellPower(25));
+
+const castFireball = (power) => `Fireball blazes with power ${power}!`,
+  summonWaterSpout = (power) => `Water spout surges with power ${power}!`,
+  callLightning = (power) => `Lightning strikes with power ${power}!`;
+const elementalSpells = [castFireball, summonWaterSpout, callLightning];
+
+for (let index = 0; index < elementalSpells.length; index++) {
+  const func = elementalSpells[index];
+  console.log(func(index + 1));
+}
+
+const determineSpellStrength = (power) => {
+  if (power < 50) {
+    return 'Weak';
+  } else if (power >= 50 && power <= 100) {
+    return 'Moderate';
+  } else {
+    return 'Strong';
+  }
+};
+
+[30, 75, 150].forEach((power) => console.log(determineSpellStrength(power)));
+
+const transformIngredients = (ingredients, transformFunc) => {
+  return ingredients.map((ingredient) => transformFunc(ingredient));
+};
+
+const result = transformIngredients(
+  ['newt eyes', 'toe of frog', 'wool of bat', 'tongue of dog'],
+  (ingredient) => `enchanted ${ingredient}`
+);
+
+console.log(result);
