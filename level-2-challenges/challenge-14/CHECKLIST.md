83b46138262e3f836be46f1f0869fd20bc1215bd
Jeremie Litzler <jeremie.litzler@gmail.com>
2025-03-21 10:51:42 +0100

    complete challenge 14 of level 2

--- level-2-challenges/challenge-14/CHECKLIST.md
+++ level-2-challenges/challenge-14/CHECKLIST.md
@@ -1,1 +1,5 @@
-- The harmony hub displayed as expected
+## Expected Output
+
+After completing the challenge, you will see Harmony Hub's Digital Audio Workstation interface:
+
+![Harmony Hub DAW Screenshot](https://raw.githubusercontent.com/JavaScript-Certification/images/refs/heads/main/images/training/7-1/harmony-hub.jpg)
--- level-2-challenges/challenge-14/src/main.js
+++ level-2-challenges/challenge-14/src/main.js
@@ -1,50 +1,71 @@
 // complete the challenge here 👇
 
-import { data } from '../public/data'
+import { data } from '../public/data';
 import {
   renderTrackMetadata,
   renderPlaylist,
   renderAudioEffects,
   renderMixParameters,
-  renderInstrumentTuning
-} from './render'
+  renderInstrumentTuning,
+} from './render';
 
 // 1. Track Metadata Formatter
-export function formatTrackMetadata (title, artist, genres) {
+export function formatTrackMetadata(title, artist, genres) {
+  const trackTitleEl = document.createElement('div');
+  trackTitleEl.classList.add('track-title');
+  trackTitleEl.innerText = title;
 
+  const trackArtistEl = document.createElement('div');
+  trackArtistEl.classList.add('track-artist');
+  trackArtistEl.innerText = title;
+
+  const trackGenresEl = document.createElement('div');
+  trackGenresEl.classList.add('track-genres');
+  trackGenresEl.innerText = genres.join(', ');
+
+  const html = `${trackTitleEl.outerHTML}${trackArtistEl.outerHTML}${trackGenresEl.outerHTML}`;
+  console.log(html);
+
+  return html;
 }
 
-renderTrackMetadata(formatTrackMetadata(data.track.title, data.track.artist, data.track.genres))
+renderTrackMetadata(
+  formatTrackMetadata(data.track.title, data.track.artist, data.track.genres)
+);
 
 // 2. Playlist Creator
-export function createPlaylist (existingPlaylist, newTracks) {
+export function createPlaylist(existingPlaylist, newTracks) {
+  console.log('createPlaylist', existingPlaylist, newTracks);
 
+  return [...existingPlaylist, [newTracks]];
 }
 
-
-renderPlaylist(createPlaylist(data.playlist, data.newTracks[0], data.newTracks[1]))
-
+renderPlaylist(
+  createPlaylist(data.playlist, data.newTracks[0], data.newTracks[1])
+);
 
 // 3. Audio Effect Merger
-export function mergeAudioEffects (defaultEffects, customEffects) {
+export function mergeAudioEffects(defaultEffects, customEffects) {
+  console.log('mergeAudioEffects', defaultEffects, customEffects);
 
+  return { ...defaultEffects, ...customEffects };
 }
 
-
-renderAudioEffects(mergeAudioEffects(data.defaultEffects, data.customEffects))
+renderAudioEffects(mergeAudioEffects(data.defaultEffects, data.customEffects));
 
 // 4. Mix Assignment
-export function assignMixParameters (params) {
-
+export function assignMixParameters(params) {
+  return { ...params, isMuted: false };
 }
 
-
-renderMixParameters(assignMixParameters(data.mixParameters))
-
+renderMixParameters(assignMixParameters(data.mixParameters));
 
 // 5. Instrument Tuning Check
-export function checkInstrumentTuning (tuningObject, instrumentName) {
+export function checkInstrumentTuning(tuningObject, instrumentName) {
+  console.log('checkInstrumentTuning', tuningObject, instrumentName);
+  return Object.hasOwn(tuningObject, instrumentName)
+    ? tuningObject[instrumentName]
+    : 440;
 }
 
-
-renderInstrumentTuning(checkInstrumentTuning(data.tuning, 'violin'))
+renderInstrumentTuning(checkInstrumentTuning(data.tuning, 'trumpet'));
