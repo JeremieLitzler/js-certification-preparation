f8f3418c5ddffe37e407f4ce5ecd4b16e379c3cb
Jeremie Litzler <jeremie.litzler@gmail.com>
2025-03-24 08:25:00 +0100

    complete challenge 17 of level 2

--- level-2-challenges/challenge-17/src/main.js
+++ level-2-challenges/challenge-17/src/main.js
@@ -1,30 +1,44 @@
 // complete the challenge here 👇
 
 // 1. Syntax Trap
-export function parseMagicSpell (spellString) {
-  const spellObject = JSON.parse(spellString)
-  return spellObject
+export function parseMagicSpell(spellString) {
+  console.log('parseMagicSpell', spellString);
+  try {
+    const spellObject = JSON.parse(spellString);
+    return spellObject;
+  } catch (error) {
+    return error;
+  }
 }
 
-console.log(parseMagicSpell('{"spell": "fireball", "power": 10,}'))
+console.log(parseMagicSpell('{"spell": "fireball", "power": 10,}'));
 
 // 2. Range Maze
-export function createMagicArray (length) {
-  return new Array(length)
+export function createMagicArray(length) {
+  try {
+    console.log('createMagicArray', length);
+
+    return new Array(length);
+  } catch (error) {
+    return error;
+  }
 }
 
-console.log(createMagicArray(-1))
+console.log(createMagicArray(-1));
 
 // 3. Reference Enigma
-export function castSpell (spellName, target) {
-  const spells = {
-    'fireball': (t) => `Cast fireball at ${t}`,
-    'heal': (t) => `Heal ${t}`,
-    'shield': (t) => `Apply shield to ${t}`
-  }
+export function castSpell(spellName, target) {
+  try {
+    const spells = {
+      fireball: (t) => `Cast fireball at ${t}`,
+      heal: (t) => `Heal ${t}`,
+      shield: (t) => `Apply shield to ${t}`,
+    };
 
-  return spells[spellName](target)
+    return spells[spellName](target);
+  } catch (error) {
+    return error;
+  }
 }
 
-
-console.log(castSpell('invisibility', 'wizard'))
+console.log(castSpell('invisibility', 'wizard'));
