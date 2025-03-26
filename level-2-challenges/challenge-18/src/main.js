5dcd5f3b3d1ca6a8022835f8a6ed5a9309b35a25
Jeremie Litzler <jeremie.litzler@gmail.com>
2025-03-26 15:59:36 +0100

    complete challenge 18 of level 2

--- level-2-challenges/challenge-18/src/main.js
+++ level-2-challenges/challenge-18/src/main.js
@@ -1,4 +1,38 @@
-export function main () {
+export function main() {
+  const timer = document.querySelector('#timer');
+  const start = document.querySelector('#start');
+  const reset = document.querySelector('#reset');
+  const pause = document.querySelector('#pause');
 
+  console.log(timer, start, reset, pause);
+
+  let time = 0;
+  let intervalId = 0;
+  const updateTimer = () => {
+    const timerContent =
+      time > 10 ? time.toString() : time.toString().padStart(2, 0);
+    timer.textContent = timerContent;
+  };
+  const startFn = (event) => {
+    updateTimer();
+    start.style.display = 'none';
+    pause.style.display = 'block';
+    intervalId = setInterval(() => {
+      time++;
+      updateTimer();
+    }, 1000);
+  };
+  const pauseFn = (event) => {
+    clearInterval(intervalId);
+    pause.style.display = 'none';
+    start.style.display = 'block';
+  };
+  const resetFn = (event) => {
+    time = 0;
+    updateTimer();
+    clearInterval(intervalId);
+  };
+  start.addEventListener('click', startFn);
+  pause.addEventListener('click', pauseFn);
+  reset.addEventListener('click', resetFn);
 }
-
